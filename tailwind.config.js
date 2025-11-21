477aa7e3fac62456c0c9ed855be66c519008fa0f
HugoSvy <hsvy78@gmail.com>
2025-11-21 17:11:03 +0100

    amélioration du design global

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       colors: {
         primary: "#2f6f4e", // ton vert doux
         accent: "#6fae86",
+        background: "#CADACBff", // ton vert très clair
+        secondary: "#e9fdecff", // ton beige clair
+        greenSoft: "#a9c99fff",
+        greenSoftLight: "#f9fdfaff",
+        orangeSoft: "#ffebdaff",
+        orangeSoftLight: "#fffbf8ff",
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
